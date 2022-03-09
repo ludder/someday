bf906152bd7034080dbaf2e4738f6956d80d6e3c
Tom Greuter <info@tomgreuter.nl>
2022-03-09 20:09:45 +0100

    Remove Next boilerplate and create some basic navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <h1>Someday Bank</h1>
+      <menu>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/create">Create account</Link></li>
+        </ul>
+      </menu>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- pages/create.tsx
+++ pages/create.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Someday Bank</title>
-        <meta name="description" content="This bank doesn't cost you any money" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <h2>Create account</h2>
+      <form action="">
+
+      </form>
     </div>
   )
 }
